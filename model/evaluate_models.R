f9c72a15902aba8088aef4cdfcfe6c5bc6bbc3c0
Min Xu <min.cut@gmail.com>
2013-08-29 15:18:34 -0500

    plots

--- model/evaluate_models.R
+++ model/evaluate_models.R
@@ -18,21 +18,27 @@ library(glmnet)
 library(randomForest)
 
 # basic analytics table
-ok.data = read.csv("/ebs/dssg-shared/data/dc_ok_data.csv")
-n<-nrow(ok.data)
+ok.data <- read.csv("../students.csv")
+n <- nrow(ok.data)
+ok.data$race <- factor(ok.data$race)
 
-# national model response (RF)
-load("/ebs/dssg-shared/RData/demo_score_rf_augment.RData")
-demo.score = demo.score.rf.augment
 
-# network 
-source("/ebs/dssg-shared/github/dcps_process/dcps_network.r")
-if (!exists("network")){
-	network = dcps_network.load_data(large_flag = FALSE)
-}
-ok.data = ok.data[ok.data$id %in% colnames(network),]
+# national demographics model score
+demo.score <- NULL
 
-source('/ebs/dssg-shared/github/dcps_model/error-analysis/analyze-error-boot.R')
+# network code  
+#source("../network/network.r")
+#if (!exists("network")){
+#	network = network.load_data(large_flag = FALSE)
+#}
+#ok.data <- ok.data[ok.data$id %in% colnames(network),]
+network <- NULL
+
+source("analyze_error.R")
+
+
+# We use "analyze-error-boot.R" to perform tests
+
 #-----#
 # Example Function Definition:
 #  input: train.x, train.y, test.x 
@@ -41,71 +47,60 @@ source('/ebs/dssg-shared/github/dcps_model/error-analysis/analyze-error-boot.R')
 #  -inputs NOT standardized, NON-zero mean
 #  -train.x has column names
 
-
 netRFFunBase <- function(train.x, train.y, test.x, options, tune=0.0001){ 
   print("Using network")
   flush.console()
 
-  stopifnot(c('train.ids', 'test.ids', 'network', 'demo.score') %in% names(options))
-  ntest = nrow(test.x)
-  train.ids = options$train.ids
-  test.ids = options$test.ids
-  train.x$demo.score = demo.score[match(train.ids, demo.score$id),2]
-  test.x$demo.score = demo.score[match(test.ids, demo.score$id),2] 
-  network = options$network
+  stopifnot(c('train.ids', 'test.ids', 'network') %in% names(options))
+  ntest <- nrow(test.x)
+  train.ids <- options$train.ids
+  test.ids <- options$test.ids
 
-  rf.obj = randomForest(x=train.x, y=train.y, ntree=500)
-  yhat_rf = predict(rf.obj, test.x)  
+  network <- options$network
+
+  rf.obj <- randomForest(x=train.x, y=train.y, ntree=500)
+  yhat_rf <- predict(rf.obj, test.x)  
  
-  net_ixs = colnames(network) %in% test.ids
+  net_ixs <- colnames(network) %in% test.ids
  
-  sm_net = network[net_ixs, net_ixs] 
-  diag(sm_net) = -apply(sm_net, 1, sum) 
-  yhat = solve(diag(ntest) + 0.000005*sm_net)%*%yhat_rf
+  sm_net <- network[net_ixs, net_ixs] 
+  diag(sm_net) <- -apply(sm_net, 1, sum) 
+  yhat <- solve(diag(ntest) + 0.000005*sm_net)%*%yhat_rf
   return(as.vector(yhat))
 }
 
 
-
-
-# a TOTAL CLUDGE to get civis undermatch prediction
-civisFun2<-function(train.x, train.y, test.x, options){
-  print("Using civis undermatch prediction (target-col_demo_test).")
-  flush.console()
-  return(target.df$target[match(test.x$composite_psat, target.df$psat_rg)] - 
-			test.x$col_demo_test)
-}
-
 linearAllFun <- function(train.x, train.y, test.x, options){
   print("Using linear with all features and Tikhon regularization.")
   flush.console()
-  cvo<-cv.glmnet(as.matrix(train.x), train.y, nfolds=10, alpha=0)
-  test.y<-predict(cvo, as.matrix(test.x), s="lambda.min")
+  cvo <- cv.glmnet(as.matrix(train.x), train.y, nfolds=10, alpha=0)
+  test.y <- predict(cvo, as.matrix(test.x), s="lambda.min")
 }
 
 linearLassoFun <- function(train.x, train.y, test.x, options){
   print("Using linear with all features and lasso selection.")
   flush.console()
-  cvo<-cv.glmnet(as.matrix(train.x),train.y,nfolds=10)
+  cvo <- cv.glmnet(as.matrix(train.x),train.y,nfolds=10)
 
-  i_min<-match(cvo$lambda.min, cvo$lambda)
-  beta_lin<-cvo$glmnet.fit$beta[,i_min]
+  i_min <- match(cvo$lambda.min, cvo$lambda)
+  beta_lin <- cvo$glmnet.fit$beta[,i_min]
+  
   #refit
-  relvars = which(abs(beta_lin) > 0.00001) #for fewer features only
+  relvars <- which(abs(beta_lin) > 0.00001) #for fewer features only
   #print(names(train.x)[relvars])
-  train = data.frame(train.y, train.x[,relvars]) 
-  glmfit = glm(train.y~., data=train)
-  test.y = predict(glmfit, data.frame(test.x[,relvars])) 
+  train <- data.frame(train.y, train.x[,relvars]) 
+  glmfit <- glm(train.y~., data=train)
+  test.y <- predict(glmfit, data.frame(test.x[,relvars])) 
   return(test.y)
 }
 
 linearGPAHonFun<- function(train.x, train.y, test.x, options){
   print("Using linear with gpa+honors.")
   flush.console()
-  relvars = c("gender", "sat", "honors", "total_gpa", "col_demo_test", "target")
-  train<-data.frame(train.y, train.x[,relvars])
-  glmfit = glm(train.y~., data=train)
-  test.y = predict(glmfit, data.frame(test.x[,relvars]))
+  relvars <- c("psat", "gpa", "target")
+  train <- data.frame(train.y, train.x[,relvars])
+  glmfit <- glm(train.y~., data=train)
+  test.y <- predict(glmfit, data.frame(test.x[,relvars]))
   return(test.y)
 }
 
@@ -113,61 +108,77 @@ randForestFun <- function(train.x, train.y, test.x, options){
   print("Using RF with all features and lasso selection.")
   flush.console()
   cvo <- cv.glmnet(as.matrix(train.x), train.y, nfolds=10)
-  i_min<-match(cvo$lambda.min, cvo$lambda)
-  beta_lin<-cvo$glmnet.fit$beta[,i_min]
+  i_min <- match(cvo$lambda.min, cvo$lambda)
+  beta_lin <- cvo$glmnet.fit$beta[,i_min]
   #refit
-  relvars = which(abs(beta_lin) > 0.00001) #for fewer features only
-  rf.obj = randomForest(x=train.x[,relvars], y=train.y, ntree=500)
-  test.y = predict(rf.obj, test.x[,relvars])   
+  relvars <- which(abs(beta_lin) > 0.00001) #for fewer features only
+  rf.obj <- randomForest(x=train.x[,relvars], y=train.y, ntree=500)
+  test.y <- predict(rf.obj, test.x[,relvars])   
 }
 
 randForestGPAHonFun <- function(train.x, train.y, test.x, options){
-  print("Using RF with honors+total_gap.")
+  print("Using RF with gpa, psat.")
   flush.console()
-  relvars = c("gender", "sat", "honors", "total_gpa", "col_demo_test", "target")
-  rf.obj = randomForest(x=train.x[,relvars], y=train.y, ntree=500)
-  test.y = predict(rf.obj, test.x[,relvars])   
-}
-
-rfrfFun <- function(train.x, train.y, test.x, options){
-  print("Using RFRF")
-  flush.console()
-  
-  stopifnot(c('train.ids', 'test.ids', 'demo.score') %in% names(options))
-  train.ids = options$train.ids
-  test.ids = options$test.ids
-  train.x$demo.score = demo.score[match(train.ids, demo.score$id),2]
-  test.x$demo.score = demo.score[match(test.ids, demo.score$id),2] 
-
-  rf.obj = randomForest(x=train.x, y=train.y, ntree=500)
-  test.y = predict(rf.obj, test.x)   
+  relvars <- c("gpa", "psat", "target")
+  rf.obj <- randomForest(x=train.x[,relvars], y=train.y, ntree=500)
+  test.y <- predict(rf.obj, test.x[,relvars])   
 }
 
 
-fun_ls = list( 
+fun_ls <- list( 
 		linearAll=linearAllFun, 
 		linearLasso=linearLassoFun, linearGPAHon=linearGPAHonFun, 
-		rfLasso=randForestFun, rfGPAHon=randForestGPAHonFun, 
-		netRF=netRFFun1, netRF2=netRFFun2, netRF3 = netRFFun3,
-		rfrf=rfrfFun)
+		rfLasso=randForestFun, rfGPAHon=randForestGPAHonFun)
 
-#- variables to keep in xs
-remove_vars = c('id', 'weight_v1', 'weight_v2', 'att_flag', 
-		'days', 'col_sat', 'resid', 
-		'col_nodemo_test', colnames(ok.data)[grep('sat_', colnames(ok.data))], 
+#- variables to keep remove in xs
+remove_vars <- c('sid', 'weight_v1', 'weight_v2', 'att_flag', 
+		'days', 'col_sat', 'resid', 'name', 'college', 'race',
+		colnames(ok.data)[grep('sat_', colnames(ok.data))], 
 		colnames(ok.data)[grep('_grade', colnames(ok.data))])
 
-var_ls = -which(colnames(ok.data) %in% remove_vars) 
+var_ls <- -which(colnames(ok.data) %in% remove_vars) 
 xs <- ok.data[,var_ls]
-xs[is.na(xs)] = 0
+xs[is.na(xs)] <- 0
 
 
 res <- analyzeError(xs, pred.undermatch=T, 
 		col_sat=ok.data$col_sat, target=ok.data$target, 
-		psat=ok.data$composite_psat, FUNS=fun_ls, 
+		psat=ok.data$psat, FUNS=fun_ls, 
 		ids=ok.data$id, ntrial=30,
 		options=list(demo.score=demo.score,
 				network=network)) 
 
-#write.table(res, file="/ebs/dssg-shared/github/dcps_model/error-analysis/augment-error.csv", col.names=T, row.names=T)
+write.table(res$df, file="../errors.csv",
+            col.names=T, row.names=T)
 
+
+# produce ROC plots
+
+nfun <- length(fun_ls)
+aucs <- rep(0, nfun)
+
+for (ii in 1:nfun){
+  cur_x = 2
+  cur_y = 1
+  ixs = 2:ncol(res$rocs[[ii]])
+  aucs[ii] <- (res$rocs[[ii]][cur_x,ixs] - res$rocs[[ii]][cur_x,ixs-1]) %*%
+                (res$rocs[[ii]][cur_y,ixs] + res$rocs[[ii]][cur_y,ixs-1])/2
+}
+
+
+theme_set(theme_grey(base_size=22))
+p_obj <- ggplot() +
+        geom_line(aes(x=res$rocs[[1]][cur_x,], y=res$rocs[[1]][cur_y,],
+                        colour="red"), width=3) +
+        geom_line(aes(x=res$rocs[[4]][cur_x,], y=res$rocs[[4]][cur_y,],
+                        colour="blue"), width=3) +
+        geom_abline(intercept=0, slope=1) +
+        scale_x_continuous("False Positive Rate") +
+        scale_y_continuous("True Positive Rate") +
+        scale_colour_manual(name="AUC", values=c("red", "blue"),
+                        labels=c(paste0("RF: ", aucs[1]),
+                          paste0("lin: ", aucs[4]) )) +
+        coord_fixed(ratio=1)
+
+pdf("../viz/roc_test.pdf")
+dev.off()
